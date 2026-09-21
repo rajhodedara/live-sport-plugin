8919738d51f54966383fe236666d7e2e7de32cc3
rajhodedara <68367766+rajhodedara@users.noreply.github.com>
2026-09-21 18:14:56 +0530

    chore: untrack dist/ build artifacts, gitignore entire dist folder

--- dist/src/resolve/slot.js
+++
@@ -1,4 +0,0 @@
-export function makeSlot(origin, source, id, stream) {
-  const streamNo = String(stream)
-  return { origin, path: `${source}/${id}/${streamNo}`, source, id, stream: streamNo, slug: id }
-}
--- dist/src/wire/headers.js
+++
@@ -1,7 +0,0 @@
-import { ua } from '../env.js'
-
-export function fetchHeaders(referer, extra = {}) {
-  const headers = { 'User-Agent': ua, ...extra }
-  if (referer) headers.Referer = referer
-  return headers
-}
